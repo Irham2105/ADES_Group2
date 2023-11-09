7b12990bf00e0aef969c4215b68c8352c0aecd1f
Irham2105 <IRHAMSHAHK.22@ichat.sp.edu.sg>
2023-11-09 14:25:57 +0800

    promise change

--- backend/src/model/products.js
+++ backend/src/model/products.js
@@ -1,55 +1,81 @@
 require('dotenv').config();
-
-const sql = require('../config/database.js')
+const sql = require('../config/database.js');
 
 const productsDB = {
-  getAllFeaturedProducts: async function () {
-    try {
-      const result = await sql`SELECT p.imageurl, p.name, p.description
-      FROM homesteadhaven.products p
-      INNER JOIN homesteadhaven.featured f ON p.productid = f.productid
-      `;
-      return result;
-    } catch (err) {
-      console.error(err);
-      throw err; // You can choose to handle the error differently here
-    }
+  getAllFeaturedProducts: function () {
+    return new Promise((resolve, reject) => {
+      sql`SELECT p.imageurl, p.name, p.description
+        FROM homesteadhaven.products p
+        INNER JOIN homesteadhaven.featured f ON p.productid = f.productid`
+        .then(result => {
+          resolve(result);
+        })
+        .catch(err => {
+          console.error(err);
+          reject(err);
+        });
+    });
   },
 
-  getAllProducts: async function () {
-    try {
-      const result = await sql`SELECT name, description, price, category, stockquantity, imageurl FROM homesteadhaven.products`;
-      return result;
-    } catch (err) {
-      console.error(err);
-      throw err; // You can choose to handle the error differently here
-    }
+  getAllProducts: function () {
+    return new Promise((resolve, reject) => {
+      sql`SELECT name, description, price, category, stockquantity, imageurl FROM homesteadhaven.products`
+        .then(result => {
+          resolve(result);
+        })
+        .catch(err => {
+          console.error(err);
+          reject(err);
+        });
+    });
   },
 
-  getProductsByCategory: async function (category) {
-    try {
-      const result = await sql`
-              SELECT p.* FROM homesteadhaven.products p INNER JOIN homesteadhaven.Category c ON p.category = c.category_id WHERE c.category_name = ${category}
-            `;
-      return result;
-    } catch (err) {
-      console.error(err);
-      throw err; // You can choose to handle the error differently here
-    }
+  getProductsByCategory: function (category) {
+    return new Promise((resolve, reject) => {
+      sql`SELECT p.* FROM homesteadhaven.products p INNER JOIN homesteadhaven.Category c ON p.category = c.category_id WHERE c.category_name = ${category}`
+        .then(result => {
+          resolve(result);
+        })
+        .catch(err => {
+          console.error(err);
+          reject(err);
+        });
+    });
   },
 
-  editProduct: async function (product_id, stockquantity) {
-    try {
-      const result = await sql `UPDATE homesteadhaven.products SET stockquantity = ${stockquantity} WHERE productid = ${product_id}`
-      return result
-    } catch (err) {
-      console.error(error)
-    }
+  editProduct: function (product_id, stockquantity) {
+    return new Promise((resolve, reject) => {
+      sql`UPDATE homesteadhaven.products SET stockquantity = ${stockquantity} WHERE productid = ${product_id}`
+        .then(result => {
+          resolve(result);
+        })
+        .catch(err => {
+          console.error(err);
+          reject(err);
+        });
+    });
+  },
+
+  deleteProduct: function (product_id) {
+    return new Promise((resolve, reject) => {
+      sql`DELETE FROM homesteadhaven.products WHERE productid = ${product_id}`
+        .then(result => {
+          resolve(result);
+        })
+        .catch(err => {
+          console.error(err);
+          reject(err);
+        });
+    });
+  },
+
+  addProduct: function (name, description, price, category, stockquantity, imageurl) {
+    return new Promise((resolve, reject) => {
+      // Implement the logic for adding a product here
+      // Use sql`...` to execute the SQL query
+      // Resolve or reject the Promise based on the result
+    });
   }
 };
-
-
-
-
 
 module.exports = productsDB;
